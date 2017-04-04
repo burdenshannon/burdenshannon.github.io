3822e1e37935c64b2ce24511041d4338de03dc9a
Shannon Burden <sburden@movableink.com>
2017-04-04 01:23:25 -0700

    Wedding Website

---
+++ RSVP-engagement.php
@@ -0,0 +1,38 @@
+<?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$guests_number = $_POST['guests'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'shannonefburden@gmail.com';//<== update the email address
+$email_subject = "RSVP for Engagement Party";
+$email_body = "Name: $name.\n".
+    "Guest: $guests_number\n
+    ".
+    
+$to = "tom@amazing-designs.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: index.html');
+
+   
+?> 
